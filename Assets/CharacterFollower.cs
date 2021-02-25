77326d932e58f26e7f8856896a0c10f42adfcfb4
Toby.Taylor <toby.taylor20@yahoo.com>
2021-02-25 10:11:26 +0000

    Initial commit

---
+++ Assets/CharacterFollower.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterFollower : MonoBehaviour
+{
+    public Transform charPosition;
+    private Vector3 camOffset = new Vector3(0, 0, -9);
+    public Transform camPosition;
+    public float followRate;
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void FixedUpdate()
+    {
+        Follow();
+    }
+
+   void Follow()
+    {
+        Vector3 targPosition = charPosition.position + camOffset;
+        Vector3 followingPosition = Vector3.Lerp(camPosition.position, targPosition, followRate * Time.fixedDeltaTime);
+        camPosition.position = followingPosition;
+    }
+
+}
+        
---
+++ Assets/GolemController.cs
@@ -0,0 +1,137 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GolemController : MonoBehaviour
+{
+  
+    public float jumpSpeed;
+    public float horizontalSpeed = 10;
+    public LayerMask whatIsGround;
+    public Transform groundcheck;
+    private float groundRadius = 0.5f;
+    public float fallMultiplier = 5.0f;
+
+    private bool grounded;
+    private bool jump;
+    private bool isRunning;
+
+    bool facingRight = true;
+
+    private float hAxis;
+
+    private Rigidbody2D theRigidBody;
+
+    private Animator theAnimator;
+
+
+    void Start()
+    {
+       
+        jump = false;
+        grounded = false;
+        isRunning = false;
+
+      
+        theRigidBody = GetComponent<Rigidbody2D>();
+        theAnimator = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        jump = Input.GetKeyDown(KeyCode.Space);
+
+        hAxis = Input.GetAxis("Horizontal");
+
+
+        theAnimator.SetFloat("hspeed", Mathf.Abs(hAxis));
+
+        Collider2D colliderWeCollidedWith = Physics2D.OverlapCircle(groundcheck.position, groundRadius, whatIsGround);
+
+   
+     
+        grounded = (bool)colliderWeCollidedWith;
+
+        theAnimator.SetBool("ground", grounded);
+
+        float yVelocity = theRigidBody.velocity.y;
+
+        theAnimator.SetFloat("vspeed", yVelocity);
+
+        theAnimator.SetBool("running", isRunning);
+
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            horizontalSpeed = 25;
+            isRunning = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            horizontalSpeed = 10;
+            isRunning = false;
+        }
+
+
+
+        if (grounded)
+        {
+            if ((hAxis > 0) && (facingRight == false))
+            {
+                Flip();
+            }
+            else if ((hAxis < 0) && (facingRight == true))
+            {
+                Flip();
+            }
+        }
+
+        if (grounded && !jump)
+        {
+            theRigidBody.velocity = new Vector2(horizontalSpeed * hAxis, theRigidBody.velocity.y);
+        }
+        else if (grounded && jump)
+        {
+
+            theRigidBody.velocity = new Vector2(theRigidBody.velocity.x, jumpSpeed);
+        }
+
+
+        if (theRigidBody.velocity.y < 0)
+        {
+
+            theRigidBody.velocity += Vector2.up * Physics2D.gravity.y * fallMultiplier * Time.deltaTime;
+
+        }
+
+    }
+
+   
+    void FixedUpdate()
+    {
+
+        
+   
+  
+
+    }
+
+
+    private void Flip()
+    {
+
+        facingRight = !facingRight;
+
+        
+        Vector3 theScale = transform.localScale;
+
+       
+        theScale.x *= -1;
+
+        
+        transform.localScale = theScale;
+    }
+
+   
+}
