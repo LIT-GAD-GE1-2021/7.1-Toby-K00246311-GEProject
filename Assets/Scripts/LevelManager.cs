8adfdc31f0be12fa8a4cd81a98a481d01f7215ad
Toby.Taylor <toby.taylor20@yahoo.com>
2021-03-18 11:06:31 +0000

    Half implemented Door and Key system

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public static LevelManager instance;
+    public GameObject door;
+    public doorController doorCon;
+    public bool hasKey;
+    private bool doorOpened;
+    private bool doorClosed = true;
+
+    public Animator doorAnimator;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void KeyCollected()
+    {
+        hasKey = true;
+
+        Debug.Log("Key Collected");
+    }
+
+    public void UsedKey()
+    {
+        hasKey = false;
+        Debug.Log("Key Used");
+    }
+
+}
---
+++ Assets/Scripts/doorController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorController : MonoBehaviour
+{
+    private Animator theAnimator;
+
+    void Start()
+    {
+        theAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
