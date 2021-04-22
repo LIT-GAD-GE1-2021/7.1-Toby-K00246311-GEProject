55f2e543241b99951e81c6b13f1d7534152c4887
Toby.Taylor <toby.taylor20@yahoo.com>
2021-04-22 10:16:13 +0100

    Enemy Implemented

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Rigidbody2D theRB;
+    public Transform tForm;
+    private bool moveLeft;
+    private bool facingRight = true;
+    private Vector2 scaleRight = new Vector2(1, 1), scaleLeft = new Vector2(-1, 1);
+
+    void Update()
+    {
+        MoveLeftAndRight();
+        Flip();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "WallR")
+        {
+            moveLeft = true;
+
+        }
+
+        if (collision.gameObject.tag == "WallL")
+        {
+            moveLeft = false;
+
+        }
+    }
+
+    public void MoveLeftAndRight()
+    {
+        if (moveLeft == false)
+        {
+            facingRight = true;
+            theRB.velocity = Vector2.right * 6;
+        }
+        if (moveLeft == true)
+        {
+            facingRight = false;
+            theRB.velocity = Vector2.left * 6;
+        }
+    }
+
+   public void Flip()
+    {
+        if (facingRight == true)
+        {
+            tForm.localScale = scaleRight;
+        }
+
+        if (facingRight == false)
+        {
+            tForm.localScale = scaleLeft;
+        }
+    }
+
+}
